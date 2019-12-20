6e1f339debbf5e0e9d28a15dc83331a6f862d363
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-12-20 21:37:59 +0800

    initial commit for ProgressBar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProgressBar = () => {
+	return (
+		<div className="ProgressBar">
+			<h1 className="ProgressBar__title">ProgressBar Demo</h1>
+		</div>
+	)
+}
+
+export default ProgressBar;
