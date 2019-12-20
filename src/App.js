e018fe38e747a34678d7d15c3175bd1b18a79816
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-12-20 21:41:01 +0800

    removed un-necessary code and imported ProgressBar component initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-
+				<ProgressBar />
 			</header>
 		</div>
 	);
