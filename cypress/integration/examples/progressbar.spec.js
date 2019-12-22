c09d11eeef7cc9a03811872b7d45d975c721a9a3
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-12-22 22:08:07 +0800

    adding more e2e test cases

--- cypress/integration/examples/progressbar.spec.js
+++ cypress/integration/examples/progressbar.spec.js
@@ -4,7 +4,7 @@ describe('HomePage', () => {
 
 	beforeEach(() => {
 		cy.visit('/')
-	})
+	});
 
 	it('should contain h1 with value progress bar', () => {
 		cy.get('.ProgressBar__title').should('contain', 'ProgressBar Demo');
@@ -16,12 +16,38 @@ describe('HomePage', () => {
 
 	it('should contain % in text vakue', () => {
 		cy.get('.Bar__value').first().should('be.visible')
-	})
+	});
 
 	it('should load atleast one bar value', () => {
 		cy.get('.ProgressBar__list>.Bar')
 			.its('length')
 			.should('be.gt', 1);
+	});
+
+	it('should contain % in text vakue', () => {
+		cy.get('.Bar__value').first().should('be.visible')
+	});
+
+	it('should increase the bar value on button click', () => {
+		let initialBarValue = cy.get('.Bar__value').first().value;
+		cy.get('.NumberButton').first().click();
+		let presentBarValue = cy.get('.Bar__value').first().value;
+		expect(initialBarValue < presentBarValue);
+	});
+
+	it('should increase the bar value on button click', () => {
+		let initialBarValue = cy.get('.Bar__value').first().value;
+		cy.get('.NumberButton').first().click();
+		let presentBarValue = cy.get('.Bar__value').first().value;
+		expect(initialBarValue !== presentBarValue);
+	});
+
+	it('change bar1 value using the dropdown and button click', () => {
+		cy.get('.ProgressBar select').select('Bar 1');
+		let initialBarValue = cy.get('.Bar__value').first().value;
+		cy.get('.NumberButton').first().click();
+		let presentBarValue = cy.get('.Bar__value').first().value;
+		expect(initialBarValue !== presentBarValue);
 	});
 
 });
