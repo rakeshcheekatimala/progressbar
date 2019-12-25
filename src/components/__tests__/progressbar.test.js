3b3108944d36d0a09299d75536719a307722ac22
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-12-25 11:34:57 +0800

    ref the testcase

--- src/components/__tests__/progressbar.test.js
+++ src/components/__tests__/progressbar.test.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BarServiceMock from "BarService";
+import BarServiceMock from "./../services/__mocks__/BarService";
 import ProgressBar from "./../ProgressBar";
-import { cleanup, render, waitForElement } from "@testing-library/react";
+import { cleanup, render, waitForElementToBeRemoved, act } from "@testing-library/react";
 
 afterEach(cleanup);
+
+beforeEach(() => {
+	BarServiceMock.getBars = jest.fn(() => Promise.resolve({
+		json: {
+			"buttons": [22, 37, -34, -22],
+			"bars": [31, 70, 89, 50],
+			"limit": 130
+		}
+	}));
+});
+
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
@@ -12,14 +23,9 @@ it("renders without crashing", () => {
 });
 
 
-it("fetches and displays data", async () => {
-	BarServiceMock.getBars.mockResolvedValueOnce({
-		json: {
-			"buttons": [22, 37, -34, -22],
-			"bars": [31, 70, 89, 50],
-			"limit": 130
-		},
+it("fetches and displays data without crashing", async () => {
+	await act(async () => {
+		const { getByTestId } = render(<ProgressBar />);
+		await waitForElementToBeRemoved(() => getByTestId('loading'));
 	});
-	const { getByTestId } = render(<ProgressBar />);
-	expect(getByTestId("loading")).toHaveTextContent("Loading data...");
 });
